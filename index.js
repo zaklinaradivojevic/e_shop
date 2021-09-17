fef9cf69d74a60e5cc1dca35bb32baee3eac0b90
zaklinaradivojevic <zaklinaradivojevic77777777@gmail.com>
2021-09-17 17:34:32 +0200

    setining server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require("./routes/user");
+
+dotenv.config();
+
+
+mongoose
+    .connect(
+        process.env.MONGO_URI
+        , {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    .then(console.log('Connected to MongoDB'))
+    .catch((error) => {
+        console.log(error);
+    });
+
+
+
+app.listen(5001, () => {
+    console.log('Server is runnong');
+})
