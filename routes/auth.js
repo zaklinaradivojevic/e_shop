5ec548613c466ef82ad5e89c2dbbf7baf6466279
zaklinaradivojevic <zaklinaradivojevic77777777@gmail.com>
2021-09-20 22:49:03 +0200

    instoling webtoken

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ router.post("/register", async (req, res) => {
         password: CryptoJS.AES.encrypt(
             req.body.password,
             process.env.PASS_SEC
-            ).toString(),
+        ).toString(),
     });
     try {
         const savedUser = await newUser.save()
@@ -17,6 +17,26 @@ router.post("/register", async (req, res) => {
     } catch (error) {
         res.status(500).json(error);
     }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+        const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        OriginalPassword !== req.body.password &&
+            res.status(401).json("Wrong credentials");
+
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
 });
 
 
